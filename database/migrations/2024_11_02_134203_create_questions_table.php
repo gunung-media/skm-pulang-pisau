4ee0ec96928e0cab6c58db374ffc098be72aacb7
Richie-Z <zakariarichie@yahoo.com>
2024-11-02 15:57:46 +0700

    feat(db): introduce migrations

---
+++ database/migrations/2024_11_02_134203_create_questions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('question');
+            $table->integer('position');
+            $table->boolean('is_active')->default(true);
+            $table->integer('number_of_answers')->default(5);
+            $table->json('custom_answers')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
