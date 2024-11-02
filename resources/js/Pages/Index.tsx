3521f20bc91613205ba46c3bca9e21e6694cad11
Richie-Z <zakariarichie@yahoo.com>
2024-11-02 16:20:00 +0700

    feat: include tailwind

--- resources/js/Pages/Index.tsx
+++ resources/js/Pages/Index.tsx
@@ -1,6 +1,6 @@
 export default function Index() {
     return (
-        <div>
+        <div className="text-3xl bg-blue-100">
             <h1>Hello World!</h1>
         </div>
     )
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
     @routes
     @viteReactRefresh
-    @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
+    @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx", 'resources/css/app.css'])
     @inertiaHead
 </head>
 
