b62a2c242e227fbde6c50c9e41371e06655ea467
Richie-Z <zakariarichie@yahoo.com>
2024-11-02 16:12:15 +0700

    feat: introduce seeder

---
+++ database/seeders/QuestionSeeder.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Repositories\QuestionRepository;
+use Illuminate\Database\Seeder;
+
+class QuestionSeeder extends Seeder
+{
+    public function __construct(
+        public QuestionRepository $questionRepository,
+    ) {}
+
+    public function run(): void
+    {
+        foreach ($this->getQuestions() as $question) {
+            $this->questionRepository->create($question);
+        }
+    }
+
+    private function getQuestions(): array
+    {
+        return [
+            [
+                'question' => 'Bagaimana pendapat Saudara tentang kesesuaian persyaratan pelayanan dengan jenis pelayanannya?',
+                'position' => 1,
+                'custom_answers' => json_encode(['Tidak sesuai', 'Kurang sesuai', 'Netral', 'Sesuai', 'Sangat sesuai']),
+            ],
+            [
+                'question' => 'Bagaimana pemahaman Saudara tentang kemudahan prosedur pelayanan di unit ini?',
+                'position' => 2,
+                'custom_answers' => json_encode(['Tidak mudah', 'Kurang mudah',  'Netral', 'Mudah', 'Sangat mudah']),
+            ],
+            [
+                'question' => 'Bagaimana pendapat Saudara tentang kecepatan waktu dalam memberikan pelayanan?',
+                'position' => 3,
+                'custom_answers' => json_encode(['Tidak cepat', 'Kurang cepat',  'Netral', 'Cepat', 'Sangat cepat']),
+            ],
+            [
+                'question' => 'Bagaimana pendapat Saudara tentang kewajaran biaya/tarif dalam pelayanan',
+                'position' => 4,
+                'custom_answers' => json_encode(['Sangat mahal', 'Cukup mahal',  'Netral', 'Murah', 'Gratis']),
+            ],
+            [
+                'question' => 'Bagaimana pendapat Saudara tentang kesesuaian produk pelayanan antara yang tercantum dalam standar pelayanan dengan hasil yang diberikan.',
+                'position' => 5,
+                'custom_answers' => json_encode(['Tidak sesuai', 'Kurang sesuai', 'Netral', 'Sesuai', 'Sangat sesuai']),
+            ],
+            [
+                'question' => 'Bagaimana pendapat Saudara tentang kompetensi/ kemampuan petugas dalam pelayanan.',
+                'position' => 6,
+                'custom_answers' => json_encode(['Tidak kompeten', 'Kurang kompeten', 'Netral', 'Kompeten', 'Sangat kompeten']),
+            ],
+            [
+                'question' => 'Bagaimana pendapat Saudara tentang perilaku petugas dalam pelayanan terkait kesopanan dan keramahan',
+                'position' => 7,
+                'custom_answers' => json_encode(['Tidak sopan dan ramah', 'Kurang sopan dan ramah', 'Netral', 'Sopan dan ramah', 'Sangat sopan dan ramah']),
+            ],
+            [
+                'question' => 'Bagaimana pendapat Saudara tentang kualitas sarana dan prasarana',
+                'position' => 8,
+                'custom_answers' => json_encode(['Buruk', 'Cukup', 'Netral', 'Baik', 'Sangat Baik']),
+            ],
+            [
+                'question' => 'Bagaimana pendapat Saudara tentang penanganan pengaduan pengguna layanan',
+                'position' => 9,
+                'custom_answers' => json_encode(['Tidak ada', 'Ada tetapi tidak berfungsi', 'Netral', 'Berfungsi kurang maksimal', 'Dikelola dengan baik']),
+            ]
+        ];
+    }
+}
