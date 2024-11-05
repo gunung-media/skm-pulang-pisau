fc449523d3a3f4497b599d8e9f7acfc14e4c31e0
Richie-Z <zakariarichie@yahoo.com>
2024-11-05 17:57:35 +0700

    feat: add error message on form component

--- resources/js/components/Form/index.tsx
+++ resources/js/components/Form/index.tsx
@@ -5,13 +5,17 @@ type props = {
     children: React.ReactNode
     label: string
     className?: string
+    error?: string
 }
 
-export const Form: FC<props> = ({ children, label, className }) => {
+export const Form: FC<props> = ({ children, label, className, error }) => {
     return (
         <div className={cn('my-5', className)}>
             <label className={cn('text-base font-base')}>{label}</label>
             {children}
+            {error && (
+                <p className="mt-2 text-red-500 text-sm">{error}</p>
+            )}
         </div>
     )
 }
