3d23119f8511e8a2b048c71912b0d3dcacb1a7da
Richie-Z <zakariarichie@yahoo.com>
2024-11-25 20:34:57 +0700

    feat: meta for seo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,18 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta name="description"
+        content="Kami mendengarkan setiap masukan dengan seksama, memahami kebutuhan Anda, dan berkomitmen untuk terus meningkatkan kualitas layanan demi kepuasan Anda.">
+    <meta name="keywords"
+        content="IKM, Pulang pisau, survey, IKM app, IKM web, layanan terbaik, mendengarkan masukan, kebutuhan pelanggan, kualitas layanan, kepuasan pelanggan">
+    <meta name="robots" content="index, follow">
+    <meta name="author" content="Gunung Media">
+    <meta property="og:title" content="{{ config('app.name', 'FeedbackHub') }}">
+    <meta property="og:description"
+        content="Kami berkomitmen untuk mendengarkan masukan, memahami kebutuhan, dan terus meningkatkan kualitas layanan demi kepuasan Anda.">
+    <meta property="og:image" content="/ss.png">
+    <meta property="og:url" content="{{ config('app.url') }}">
+    <meta property="og:type" content="website">
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
     <title inertia>{{ config('app.name', 'FeedbackHub') }}</title>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
